8b761118eb4004866b911afac61e5cbfcfa69b30
Gaston Roberto <robertga@basfad.basf.net>
2021-11-20 17:42:28 -0300

    Clase 2 repaso

---
+++ Clase2/Ejercicio2.js
@@ -0,0 +1,11 @@
+ console.log("Comienza el programa...");
+
+ setTimeout(() => {
+     console.log("Se invoca el setTimeOut");
+ }, 1000);
+
+ setTimeout(() => {
+    console.log("Segundo setTimeOut");
+}, 999);
+
+ console.log("Termina el programa");
